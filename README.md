3de7f5701f5019cfed2f1554febb11fa4278125d
Yonle <yonle@lecturify.net>
2024-07-24 11:49:42 +0700

    prompt config maker when not found
    Signed-off-by: Yonle <yonle@lecturify.net>

--- README.md
+++ README.md
@@ -7,41 +7,9 @@ You will need to install [Go](https://go.dev) on your system. After installation
 go install github.com/Yonle/bostr2@latest
 ```
 
-Then create a YAML configuration file that looks like this:
-```yaml
----
-listen: localhost:8080
-
-favicon:
-
-relays:
-- wss://relay.example1.com
-- wss://relay.example2.com
-
-nip_11:
-  name: bostr2
-  description: Noetr relay bouncer
-  software: git+https://github.com/Yonle/bostr2
-  pubkey: 0000000000000000000000000000000000000000000000000000000000000000
-  contact: unset
-  supported_nips:
-  - 1
-  - 2
-  - 9
-  - 11
-  - 12
-  - 15
-  - 16
-  - 20
-  - 22
-  - 33
-  - 40
-  - 50
-```
-
-Save the configuration file as config.yaml. Once completed, you can run bostr2 using the following command:
+Then run:
 ```
 bostr2
 ```
 
-You will then need to manually configure a reverse proxy on your server (eg [nginx](https://nginx.org) or [caddy](https://caddyserver.com)).
+You may need to manually configure a reverse proxy on your server (could be [nginx](https://nginx.org) or [caddy](https://caddyserver.com)).
--- config.go
+++ config.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	_ "embed"
 	"fmt"
+	"log"
 	"github.com/goccy/go-yaml"
 	"os"
+	"strings"
 )
 
 type Config struct {
@@ -13,9 +17,50 @@ type Config struct {
 	Favicon string
 }
 
+//go:embed config.example.yaml
+var exampleConf []byte
+
+func createConf(filename string) {
+	fmt.Printf("Do you want to create config file? (%s) [y/N]: ", filename)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	v := scanner.Text()
+
+	switch strings.ToLower(v) {
+	case "y":
+		if err := os.WriteFile(filename, exampleConf, 0644); err != nil {
+			panic(fmt.Sprintf("failed to create config file: %s", err))
+		}
+
+		fmt.Printf("\nConfig file has been written to %s\n", filename)
+		fmt.Println("Please edit the config before starting bouncer.")
+		fmt.Println("\nAfter editing, Start the bouncer by running:")
+
+		if filename == "config.yaml" {
+			fmt.Println("  $ bostr2")
+		} else {
+			fmt.Printf("  $ bostr2 -configfile %s\n", filename)
+		}
+
+		os.Exit(0)
+		return
+	default:
+		fmt.Println("bostr2 cannot run without config.")
+		os.Exit(1)
+		return
+	}
+}
+
 func ReadConfig(filename string, c *Config) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
+		if os.IsNotExist(err) {
+			log.Println("Config file does not exist.")
+			createConf(filename)
+		}
+
 		panic(fmt.Sprintf("error when reading %s: %s", filename, err))
 	}
 
